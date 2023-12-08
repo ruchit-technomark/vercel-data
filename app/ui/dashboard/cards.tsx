44a415c5d6747f3b1c0a4c2db7b522b064af74a1
ruchit.jivnani <ruchit.jivnani@technomark.io>
2023-12-08 11:01:36 +0530

    SECOND commit

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,8 +4,6 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -48,8 +46,6 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
